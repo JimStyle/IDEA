1bf5e96d6cd3770b953d1dc5f8d68c9498cd5297
arteman <arteman@parus.com.ua>
2016-11-09 16:18:56 +0200

    Added new types of cars

--- IdeaProjects/String Manipulation/src/com/company/Main.java
+++ IdeaProjects/String Manipulation/src/com/company/Main.java
@@ -1,9 +1,18 @@
 package com.company;
 
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
         System.out.println("yo");
+        ArrayList<String> arr = new ArrayList<>();
+        arr.add("Audi");
+        arr.add("BMW");
+        arr.add("Mercedes");
+        for (String s: arr)
+            System.out.println(s);
+        arr.add("Ford");
     }
 }
