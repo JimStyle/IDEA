39a2365f31c9e6e6503b06b01daaece4f1152130
arteman <arteman@parus.com.ua>
2016-11-09 16:29:28 +0200

    Exception added

--- ArrayList/src/com/company/Main.java
+++ ArrayList/src/com/company/Main.java
@@ -6,8 +6,12 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        ArrayList<String> arr = new ArrayList<>();
-        arr.add("Audi");
-        System.out.println(arr.get(0));
+        try {
+            ArrayList<String> arr = new ArrayList<>();
+            arr.add("Audi");
+            System.out.println(arr.get(0));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
