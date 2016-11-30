fd45b556899197388e59cc0eab926530945721b9
arteman <arteman@parus.com.ua>
2016-11-30 12:16:45 +0200

    Java Generics changes

--- Java Generics/src/com/company/Main.java
+++ Java Generics/src/com/company/Main.java
@@ -1,29 +1,26 @@
 package com.company;
 
 
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        Stock <Integer> car1 = new Stock<>("Audi",5,1);
-        Stock <String> car2 = new Stock<>("BMW",4,"1");
+        ArrayList<Integer> arr = new ArrayList<>();
+        arr.add(1);
+        arr.add(2);
+        arr.add(3);
+
+        Stock <Integer,ArrayList<Integer>> car1 = new Stock<>("Audi",5,1,arr);
+        Stock <Double,ArrayList<Integer>> car2 = new Stock<>("BMW",4,1.2,arr);
+        System.out.println(car1.getQuantity());
+        System.out.println(car2.getQuantity());
+        System.out.println(car2.getGearbox());
         car2.compare(car1);
 
-        String s = "test" ;
-        //char [] s2 = new char [4];
-        int start = 4 ;
-        int end = 8 ;
-        String s2 = new String("");
-        //System.out.println(s2);
-        System.out.println(s.equals(s2));
-        System.out.println(s == s2);
-        System.out.println(s.compareTo(s2));
-        System.out.println(s.indexOf('e'));
-        System.out.println(s.substring(1,3));
-        System.out.println(s2 == null);
-        System.out.println(s.replace("e","r"));
-        System.out.println(Integer.toString(end));
-        System.out.println(System.nanoTime());
+
+
 
 
 
--- Java Generics/src/com/company/Stock.java
+++ Java Generics/src/com/company/Stock.java
@@ -4,15 +4,33 @@ package com.company;
 /**
  * Created by Arteman on 05.09.16.
  */
-public class Stock <T> extends Vehicle {
-    T quantity;
 
-    public Stock(String name, int engineSize, T quantity) {
+
+public class Stock <T extends Number,U> extends Vehicle {
+    //даем доступ только для типов наследуемых от класса Number
+    private T quantity;
+    private U gearbox;
+
+    public Stock(String name, int engineSize, T quantity, U gearbox) {
         super(name, engineSize);
+        int n = 1;
+        this.quantity = quantity;
+        this.gearbox = gearbox;
+    }
+
+    public U getGearbox() {
+        return gearbox;
+    }
+
+    public T getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(T quantity) {
         this.quantity = quantity;
     }
 
-    public void compare (Stock<?> o){
+    public void compare (Stock<?,?> o){
         if (o.quantity == this.quantity)
             System.out.println("Engines are equal");
         else
